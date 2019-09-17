c403bbde5df6a04d9f43a9939d600eb56821f101
Patrick Schön <patrick.schoen@egotec.com>
2019-09-17 10:00:32 +0200

    default zoom == 0 URL an github angepasst url je nach dem ob lokal oder online

--- ui/index.js
+++ ui/index.js
@@ -7,6 +7,13 @@ import {transform} from 'ol/proj.js';
 const centerLng = 9.13;
 const centerLat = 49.34;
 
+let url = "/hot/{z}/{x}/{y}.png";
+
+if (window.location.port == 1234) {
+    // local with domain
+    url = "https://maps.egotec.com" + url;
+}
+
 const map = new Map({
     target: 'map',
     layers: [
@@ -14,16 +21,16 @@ const map = new Map({
             source: new OSM(
                 {
                     attributions: [
-                        'All maps © <a href="https://community.egotec.com/">EGOTEC GmbH</a>',
+                        'All maps © <a href="https://github.com/EGOTEC-GmbH/openstreetmap">EGOTEC GmbH</a>',
                     ],
                     opaque: false,
-                    url: 'https://maps.egotec.com/hot/{z}/{x}/{y}.png'
+                    url: url
                 }
             )
         })
     ],
     view: new View({
         center: transform([centerLng, centerLat], 'EPSG:4326', 'EPSG:3857'),
-        zoom: 6
+        zoom: 0
     })
 });
